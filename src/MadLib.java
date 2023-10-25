b00cad68cff6a01f1ece8911da70c37f7d1b9de6
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2023-10-25 08:41:33 -0400

    Made basic madlib class

---
+++ src/MadLib.java
@@ -0,0 +1,12 @@
+public class MadLib {
+
+
+    public static void main(String[] args) {
+        new MadLib();
+    }
+
+    public MadLib(){
+        System.out.println("hello world");
+
+    }
+}
